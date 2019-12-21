f2a49b19624277b94e6b0f45feeb49a1f94f4647
Srishti Grover <grover.srishti@gmail.com>
2019-12-20 21:09:34 -0500

    Create compute_optimal_k.py
    finding optimal value for number of topics

---
+++ LDA-mallet/compute_optimal_k.py
@@ -0,0 +1,36 @@
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    id2word = dictionary
+    coherence_values = []
+    model_list = []
+    mallet_path = 'mallet-2.0.8/bin/mallet'
+    for num_topics in range(start, limit, step):
+        model = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=num_topics, id2word=id2word)
+        model.save('models/ldamodel_'+str(num_topics)+'.lda')
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+
